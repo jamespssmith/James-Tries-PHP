eac14fc273a131f55a392684043bd1ef5676274f
jamespssmith <jstansfieldsmith@gmail.com>
2017-08-14 13:14:59 +1000

    Add files via upload

---
+++ activities/activity2-2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title></title>
+	</head>
+	<body>
+<?php
+
+ 
+
+$currMonth=date (link is external)('F', time (link is external)());
+
+if ($currMonth == 'August'){
+
+  echo "<p>It's August, so it's really hot.</p>";
+
+}else{
+
+  echo "<p>Not August, so at least not in the peak of the heat.</p>";
+
+}
+
+ 
+
+?>
+	</body>
+</html>
